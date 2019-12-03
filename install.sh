1cd0cb21753a9835a09dce7d60b7cf3c9aedf3b9
GautheyValentin <beian77@hotmail.fr>
2019-12-03 21:04:22 +0100

    Fix install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt update
-sudo apt install gcc build-essential libpcre3 libpcre3-dev libssl-dev zlib1g zlib1g-dev
+apt update
+apt install gcc build-essential libpcre3 libpcre3-dev libssl-dev zlib1g zlib1g-dev
 
 path = $(pwd)
 
@@ -16,10 +16,10 @@ cd $path/nginx/nginx-1.14.2
 
 ./configure --with-http_ssl_module --add-module=../nginx-rtmp-module-master
 make
-sudo make install
-sudo rm /usr/local/nginx/conf/nginx.conf
+make install
+rm /usr/local/nginx/conf/nginx.conf
 
-sudo cp $path/conf/nginx.conf /usr/local/nginx/conf/nginx.conf
+cp $path/conf/nginx.conf /usr/local/nginx/conf/nginx.conf
 
 mkdir /mnt/hls
 mkdir /mnt/hls/server-one
