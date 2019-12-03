c60b17002d4599481bd59427907f55d8d0794c6a
GautheyValentin <beian77@hotmail.fr>
2019-12-03 20:59:52 +0100

    Fix path

--- install.sh
+++ install.sh
@@ -2,24 +2,24 @@
 sudo apt update
 sudo apt install gcc build-essential libpcre3 libpcre3-dev libssl-dev zlib1g zlib1g-dev
 
-$PWD = $(pwd)
+path = $(pwd)
 
-mkdir $PWD/nginx
-cd $PWD/nginx
+mkdir $path/nginx
+cd $path/nginx
 wget http://nginx.org/download/nginx-1.14.2.tar.gz
 tar -zxvf nginx-1.14.2.tar.gz
 
 wget https://github.com/arut/nginx-rtmp-module/archive/master.zip
 unzip master.zip
 
-cd $PWD/nginx/nginx-1.14.2
+cd $path/nginx/nginx-1.14.2
 
 ./configure --with-http_ssl_module --add-module=../nginx-rtmp-module-master
 make
 sudo make install
-rm /usr/local/nginx/conf/nginx.conf
+sudo rm /usr/local/nginx/conf/nginx.conf
 
-cp $PWD/conf/nginx.conf /usr/local/nginx/conf/nginx.conf
+sudo cp $path/conf/nginx.conf /usr/local/nginx/conf/nginx.conf
 
 mkdir /mnt/hls
 mkdir /mnt/hls/server-one
